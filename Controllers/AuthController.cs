1fa4b41bdcebdf108984d5e66092b2945b20914d
Salvador M <chava_r17@hotmail.com>
2024-11-07 23:30:27 -0600

    Updated authcontroller with the new authentication schema

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,91 +1,57 @@
-using System.Diagnostics;
+using System;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SicemOperation.Entities;
-using SicemOperation.Models;
-using SicemOperation.Services;
 
 namespace SicemOperation.Controllers;
 
-public class AuthController(ILogger<AuthController> _logger, AuthService _authService) : Controller
+public class AuthController(ILogger<AuthController> _logger) : Controller
 {
     private readonly ILogger<AuthController> logger = _logger;
-    private readonly AuthService authService = _authService;
-
-
-
+    
+    [AllowAnonymous]
     public IActionResult Login() {
-        return View(new LoginRequest());
-    }
-
-    [HttpPost]
-    public async Task<ActionResult> Login(LoginRequest request){
-        if(! ModelState.IsValid ){
-            return View(request);
+        if(!this.User.Identity!.IsAuthenticated){
+            // trigger the authentication process
+            return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
         }
-        var user = this.authService.ValidateUser(request);
-        if( user == null){
-            ViewBag.Message = "Usuario y/o contraseña incorrectos.";
-            return View(request);
-        }
-
-        await LoginUser(user);
-
         return RedirectToAction("Index", "Home");
     }
-    
+
+    [AllowAnonymous]
     public async Task<ActionResult> Logout() {
-        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        return RedirectToAction("Login");
-    }
+        if(!this.User.Identity!.IsAuthenticated){
+            return this.Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+        }
 
+        var idToken = await this.HttpContext.GetTokenAsync("id_token");
+        var authResult = this.HttpContext.Features.Get<IAuthenticateResultFeature>()?.AuthenticateResult;
+        var tokens = authResult!.Properties!.GetTokens();
+        var tokenNames = tokens.Select( token => token.Name).ToArray();
+        this.logger.LogInformation("Token Names: {TokenNames}", string.Join(", ", tokenNames));
 
-    #region private functions
-    private async Task LoginUser(Usuario user){
-        var claims = new List<Claim> {
-            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new(ClaimTypes.Email, user.Correo),
-            new(ClaimTypes.Name, user.Nombre),
-            new(ClaimTypes.Role, "Administrator"),
-            new(ClaimTypes.Actor, $"https://ui-avatars.com/api/?name={user.Nombre!.Replace(" ", "+")}&color=333&rounded=true"),
-        };
-
-        var claimsIdentity = new ClaimsIdentity(
-            claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-        var authProperties = new AuthenticationProperties
-        {
-            //AllowRefresh = <bool>,
-            // Refreshing the authentication session should be allowed.
-
-            //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-            // The time at which the authentication ticket expires. A 
-            // value set here overrides the ExpireTimeSpan option of 
-            // CookieAuthenticationOptions set with AddCookie.
-
-            IsPersistent = true,
-            // Whether the authentication session is persisted across 
-            // multiple requests. When used with cookies, controls
-            // whether the cookie's lifetime is absolute (matching the
-            // lifetime of the authentication ticket) or session-based.
-
-            //IssuedUtc = <DateTimeOffset>,
-            // The time at which the authentication ticket was issued.
-
-            //RedirectUri = <string>
-            // The full path or absolute URI to be used as an http 
-            // redirect response value.
-        };
-
-        await HttpContext.SignInAsync(
+        return SignOut(
+            new AuthenticationProperties {
+                RedirectUri = "/Home/Index",
+                Items = { { "id_token_hint", idToken } }
+                
+            },
             CookieAuthenticationDefaults.AuthenticationScheme,
-            new ClaimsPrincipal(claimsIdentity),
-            authProperties);
-
-        logger.LogInformation("User {Email} logged in at {Time}.", user.Correo, DateTime.UtcNow);
+            OpenIdConnectDefaults.AuthenticationScheme
+        );
     }
-    #endregion
+
+    [AllowAnonymous]
+    public IActionResult AccessDenied() => this.RedirectToAction("AccessDenied", "Home");
+
+    [Route("/auth/user/me")]
+    public IActionResult GetData(){
+        return Ok(
+            this.User.Claims.ToDictionary(c => c.Type, c => c.Value)
+        );
+    }
 
 }

34fd74e9a73297e6d31a3f448458cce664d3a8a4
Salvador M <chava_r17@hotmail.com>
2025-02-04 23:46:16 -0600

    Some changes

--- Controllers/CFERecordController.cs
+++ Controllers/CFERecordController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq.Expressions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SicemOperation.Data;
 using SicemOperation.Entities;
 using SicemOperation.Models;
@@ -30,77 +31,88 @@ public class CFERecordController : Controller
     }
 
     [Route("Consumo")]
-    public IActionResult Consumo([FromQuery] int? af, [FromQuery] int? mf, [FromQuery] int? m)
+    public IActionResult Consumo([FromQuery] int? af, [FromQuery] int? mf, [FromQuery] int? medidorId)
     {
+        // * get the catalog of medidores
+        var medidoresListItems = this.sicemOperationContext.CFEMedidores.Select(item => new SelectListItem(item.NumMedidor, item.Id.ToString())).ToList();
+
         // * get the period of the consumo
         var period = DateTime.Now;
-        if(af != null && mf != null)
+        try
         {
-            period = new DateTime(af.Value, mf.Value, 1);
+            if(af != null && mf != null)
+            {
+                period = new DateTime(af.Value, mf.Value, 1);
+            }
+        }
+        catch (System.Exception)
+        {
+            ViewBag.ErrorMessage = "El periodo seleccionado no es válido.";
+            return View( new ConsumoDTO()
+                {
+                    Af = period.Year,
+                    Mf = period.Month,
+                    MedidoresListItems = medidoresListItems
+                }
+            );
         }
 
         // * check if the medidor is passed
         CFEMedidor? medidor = null;
-        if(m != null)
+        if(medidorId != null)
         {
-            medidor = this.sicemOperationContext.CFEMedidores.Find(m);
+            medidor = this.sicemOperationContext.CFEMedidores.Find(medidorId);
         }
 
         // * attempt to get the consume of the medidor in the target period
         CFEConsumo? cFEConsumo = null;
-        if(medidor !=null)
+        if(medidor != null)
         {
             cFEConsumo = this.sicemOperationContext.CFEConsumos.FirstOrDefault( item => item.IdMedidor == medidor.Id && item.Af == period.Year && item.Mf == period.Month);
         }
 
-        if(cFEConsumo == null)
+        if(cFEConsumo == null & medidor != null)
         {
             cFEConsumo = new CFEConsumo {
-                IdMedidor = (medidor?.Id ?? 0),
+                IdMedidor = medidor!.Id,
                 FechaInicio = new DateTime(period.Year, period.Month, 1),
                 FechaFin = new DateTime(period.Year, period.Month, DateTime.DaysInMonth(period.Year, period.Month)),
-                Consumo = 666,
-                Importe = 666,
-                Demanda = 666,
+                Consumo = 0,
+                Importe = 0,
+                Demanda = 0,
                 Af = period.Year,
                 Mf = period.Month,
-                Kvarh = 666,
-                FpPorc = 666,
-                FpMon = 666,
-                DapMon = 666,
+                Kvarh = 0,
+                FpPorc = 0,
+                FpMon = 0,
+                DapMon = 0,
             };
         }
 
         // * prepare the view
         var record = new ConsumoDTO()
         {
+            Af = period.Year,
+            Mf = period.Month,
+            MedidoresListItems = medidoresListItems,
             Medidor = medidor,
             Consumo = cFEConsumo
         };
+        ViewBag.FormTitle = "Registrar Consumo del Periodo " + period.ToString("MMMM yyyy") + " para el Medidor " + (medidor != null ? medidor.NumMedidor : "No seleccionado");
         return View(record);
     }
 
-
-    #region partial views
-    [Route("Medidores")]
-    public ActionResult GetMedidoresGrid(int year, int month ){
-
-        // * get the incidents of the month
-        try
-        {
-            var medidores = this.sicemOperationContext.CFEMedidores.ToList();
-            return PartialView("~/Views/CFERecord/Partials/MedidoresList.cshtml", medidores);
-
-        }
-        catch(Exception err)
+    [HttpPost("store")]
+    public IActionResult StoreConsumo([FromForm] ConsumoDTO consumoDTO)
     {
-            this._logger.LogError(err, "Fail at get the incident grid data");
-            ViewData["ErrorTitle"] = "Error resumen incidencias por mes";
-            ViewData["ErrorMessage"] = "Hubo un error al obtener el resumen de incidencias, intente de nuevo o comuníquese con un administrador.";
-            return PartialView("~/Views/Shared/ErrorAlert.cshtml", new ErrorViewModel());
-        }
+        // if(!ModelState.IsValid)
+        // {
+        //     return View("Consumo", consumoDTO);
+        // }
+
+        Console.WriteLine("ConsumoDTO: {0}", consumoDTO);
+        return RedirectToAction("Consumo", new { af = consumoDTO.Af, mf = consumoDTO.Mf, medidorId = consumoDTO.MedidorId });
     }
 
-    #endregion
 
 }
--- Models/DTO/CFE/ConsumoDTO.cs
+++ Models/DTO/CFE/ConsumoDTO.cs
@@ -1,15 +1,22 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SicemOperation.Entities;
 
 namespace SicemOperation.Models.DTO.CFE
 {
     public class ConsumoDTO
     {
+        [Display(Name = "Año")]
+        [Range(2000, 2025)]
         public int Af { get; set; }
+
+        [Display(Name = "Mes")]
+        [Range(1, 12)]
         public int Mf { get; set; }
         
         [Display(Name = "Medidor")]
         public int MedidorId { get; set; }
+        public IEnumerable<SelectListItem> MedidoresListItems { get; set; } = default!;
 
         public CFEMedidor? Medidor {get;set;}
         public CFEConsumo? Consumo {get;set;}
